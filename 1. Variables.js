531cef240f808865d150051879689cd6d7f33d6d
arjunuvladimer <arjunuvladimer@gmail.com>
2022-06-08 19:49:19 +0530

    variables

---
+++ 1. Variables.js
@@ -0,0 +1,33 @@
+// Declaration
+var a
+// Intialization
+a = 10
+// Definition
+a = "dfsjlasd"
+a = true
+
+// Data Types
+// Primitive Data Types
+// 1. Number
+var b = 10 // Int, float
+// 2. String or Character
+var c = "hello world" // => 'c'
+// 3. Boolean
+var d = true
+// 4. Null
+var e = null // Intentionally saying that it is not going to have value
+// 5. Undefined
+var f = undefined // You may give value at some point
+// Non Primitive Data Types
+// 1. Array
+var g = [1,null,"hello",true,5] // Javascript engine decides the index postion
+// Structured Data
+console.log(g[2])
+// 2. Object
+// Unstructured Data
+var obj = {
+    fname: "arjun",
+    lname: "sn",
+    id: 123
+}
+
