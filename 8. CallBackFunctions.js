3d2570d540d6b28fef0c8610a38aa47b52f079af
arjunuvladimer <arjunuvladimer@gmail.com>
2022-06-10 20:25:08 +0530

    Call back functions

---
+++ 8. CallBackFunctions.js
@@ -0,0 +1,20 @@
+// Call Back Functions
+
+function messaging(fnSms,fnEmail){
+    console.log(`Sending Messages `)
+    fnSms() // Functional Call of fnSMS
+    fnEmail() // Functional Call of fnEmail
+} // Function Definition messaging
+
+messaging(
+    function fnSms(){
+        console.log("SMS Function") // Function Definition of fnSMS
+    },
+    function fnEmail(){
+        console.log("Email Function") // Function Defintion of fnEmail
+    }
+)// Functional Call
+
+
+
+
