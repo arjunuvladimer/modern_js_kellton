bc70cd752cf3be63d3dc8e78adb2b84a93bd5b04
arjunuvladimer <arjunuvladimer@gmail.com>
2022-06-14 19:20:08 +0530

    Array Methods

--- 4. Arrays.js
+++ 4. Arrays.js
@@ -115,11 +115,88 @@ console.log(`Join Method: ${ newJoinArray}`)
 
 // 7. Split Method
 // Syntax: String.split() // Return an Array
-var stringWithCommas = "I, want, to, come, home"
-var wordArray = stringWithCommas.split(",")
+var stringWithCommas = "I , want, to, come, home"
+var wordArray = stringWithCommas.split(" , ")
 console.log(`Split Method: ${ wordArray}`)
 
 // 8. Last Index of 
 // Syntax: Array.lastindexof(searchElement)
-var indexLast = [12,5,8,9,6]
+var indexLast = [12,5,8,9,6,7,9]
 console.log(`Last Index of Method: ${indexLast.lastIndexOf(8)}`)
+
+// 9. Map Method (*****)
+// Syntax: Array.map(callback)
+var arrayForMap = [1,2,3,4,5,5,6]
+var newArrayForMap = arrayForMap.map((element) => element * 2)
+console.log(`Map Method: ${newArrayForMap}`)
+
+// - Every => boolean value
+// - Filter => filters and returns filtered values array
+// - For Each => iteration gives output for every iteration
+// - Map => iteration gives output atlast
+// - Some => returns a boolean value just like every
+// - Find => stops iteration when there is a true element that shows up
+
+// 10. Pop Method
+// Pops the last element in the array
+console.log(["me",'him',"you"].pop())
+
+// 11. Push Method
+// Pushes a new element to the existing array and also changes the index position
+var pushArray =["william","john","chris","david"] 
+var indexPositionPushed = pushArray.push('Larry')
+console.log(`Push Method: ${indexPositionPushed}`)
+
+// 12. Reduce Method // Memoization Technique
+// Array.reduce(callback) // Left to Right
+var arrayReduce = [12,1,2,3].reduce((left,right) => left+right)
+// 12+1=>13 +  13+2=>15  15+3
+console.log(`Reduce Method: ${arrayReduce}`)
+
+// 13. Reduce Right // Right to Left
+// Array.reduceRight(callback)
+var arrayReduceRight = [0,2,-5,5].reduceRight((right,left) => right - left)
+console.log(`Reduce Right Method: ${arrayReduceRight}`)
+
+// 14. Reverse // reverse an array // index positions also get changed
+// Array.reverse
+var arrayReverse = [1,23,4,5,6,7]
+console.log(`Array Reverse: ${arrayReverse.reverse()}`)
+
+// 15. Shift // Index Position is changed
+var arrayShift = [1,2,3,4,5]
+var newArrayShift = arrayShift.shift()
+console.log(`Shift Array: ${arrayShift}`)
+
+// 16. Slice 
+var arraySlice = [7,1,23,13,123,187]
+var sliceArray = arraySlice.slice(2,5)
+console.log(`Slice Method: ${sliceArray}`)
+
+// 17. Splice
+var arraySplice = [7,1,23,13,123,187]
+var splicedArray = arraySplice.splice(2,3,'javascript','es6')
+console.log(`Spliced Method: ${arraySplice}`)
+
+// 18. Unshift
+var arrayUnshift = [10,20,30,40,50,70,80]
+arrayUnshift.unshift(2,5)
+console.log(arrayUnshift)
+
+// 19. Some
+// Syntax: Array.some(callback())
+var arraySome = [12,5,8,1,2].some((element, index) => element >=10)
+console.log(`Array Some Method: ${arraySome}`)
+
+// 20. Sort
+var arraySort = ['e','b','c','d','a']
+console.log(`Array Sort ${arraySort.sort()}`)
+
+// 21. toString & toLocaleString
+console.log(`To String Method ${typeof [1,2,3,4,5].toLocaleString()}`)
+
+// ES6 Methods
+// 22.  Find // Whenever it is true it is going to stop the iteration of elements
+var num = [0,2,3,4,5]
+var oddNumber = num.find((element) => element %2 == 1)
+console.log(`Find Method: ${oddNumber}`)
